d96c35d8a6be3d41379fb27a15eb27397c6dbd6b
wangqs <wqsacy@vip.qq.com>
2021-03-30 13:49:50 +0800

    初始化

---
+++ src/Client.php
@@ -0,0 +1,95 @@
+<?php
+
+	namespace Wangqs\PexelsSdk;
+
+	/**
+	 * @author Emil Kilhage <emil@glooby.com>
+	 */
+	class Client
+	{
+		/**
+		 * @var string
+		 */
+		private $token;
+
+		/**
+		 * @var \GuzzleHttp\Client
+		 */
+		private $client;
+
+		/**
+		 * @param string $token
+		 */
+		public function __construct ( $token ) {
+			$this->token = $token;
+		}
+
+
+		/**
+		 * @author     :  Wangqs  2021/3/30
+		 * @description:    图片搜索
+		 */
+		public function search ( $query , $size = 15 , $page = 1 ) {
+			return $this->getClient()
+			            ->get( 'search?' . http_build_query( [
+					            'query'    => $query ,
+					            'per_page' => $size ,
+					            'page'     => $page
+				            ] ) );
+		}
+
+		/**
+		 * @return \GuzzleHttp\Client
+		 */
+		private function getClient () {
+			if ( null === $this->client ) {
+				$this->client = new \GuzzleHttp\Client( [
+					'base_uri' => 'https://api.pexels.com/v1/' ,
+					'headers'  => [
+						'Authorization' => $this->token
+					]
+				] );
+			}
+
+			return $this->client;
+		}
+
+		/**
+		 * @author     :  Wangqs  2021/3/30
+		 * @description:    图片精选
+		 */
+		public function curated ( $size = 15 , $page = 1 ) {
+			return $this->getClient()
+			            ->get( 'curated?' . http_build_query( [
+					            'per_page' => $size ,
+					            'page'     => $page
+				            ] ) );
+		}
+
+		/**
+		 * @author     :  Wangqs  2021/3/30
+		 * @description:    视频搜索
+		 */
+		public function videoSearch ( $query , $size = 15 , $page = 1 ) {
+			return $this->getClient()
+			            ->get( 'videos/search?' . http_build_query( [
+					            'query'    => $query ,
+					            'per_page' => $size ,
+					            'page'     => $page
+				            ] ) );
+		}
+
+		/**
+		 * @author     :  Wangqs  2021/3/30
+		 * @description:    视频精选
+		 */
+		public function videoCurated ( $size = 15 , $page = 1 ) {
+			return $this->getClient()
+			            ->get( 'videos/popular?' . http_build_query( [
+					            'per_page' => $size ,
+					            'page'     => $page
+				            ] ) );
+		}
+
+
+	}
